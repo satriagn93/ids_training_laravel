a65f99123037b48e26eb83805fa05d3c329dd8d6
satria girinanda <satriagn93@gmail.com>
2023-07-22 11:20:58 +0700

    ORM

---
+++ database/migrations/2023_07_22_033931_create_karyawan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address');
+            $table->string('dob');
+            $table->string('name');
+            $table->string('status');
+            $table->integer('karyawan_detail_id');
+            $table->date('updated_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};
